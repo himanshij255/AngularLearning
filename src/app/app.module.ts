d471538e2f62a54a97c2e36bf8035f10c02a93de
Himanshi Jain <himanshij255@gmail.com>
2022-03-04 18:12:29 +0530

    SPA implementation done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PromotionService } from './services/promotion.service';
+
 
 @NgModule({
   declarations: [
@@ -42,7 +44,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 	MatListModule,
 	AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService,PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
