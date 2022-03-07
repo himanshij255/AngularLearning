eb2f6925166b6969e4daec6adeb8ae74b3ddf4f2
Himanshi Jain <himanshij255@gmail.com>
2022-03-07 11:51:16 +0530

    Template Driven Form part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,10 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -46,7 +50,11 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatButtonModule,
 	MatListModule,
 	MatDialogModule,
-	AppRoutingModule
+	AppRoutingModule,
+	MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+	FormsModule 
   ],
   entryComponents: [
         LoginComponent
